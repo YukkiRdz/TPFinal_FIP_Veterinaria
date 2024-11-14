96d670777f4547a003ee03035de2880310f1bb66
Jeremias Mastroianni <165935511+MastroianniJeremias@users.noreply.github.com>
2024-11-13 21:01:34 -0300

    Clase RedVeterinaria + arrays + method

---
+++ Veterinaria/RedVeterinaria.ts
@@ -0,0 +1,13 @@
+class RedVeterinaria {
+    private arrayVeterinarias: Veterinaria[] = [];
+    private arrayProveedores: Proveedor[] = [];
+  
+    listadoVeterinarias(): Veterinaria[] {
+      return this.arrayVeterinarias;
+    }
+  
+    listadoProveedores(): Proveedor[] {
+      return this.arrayProveedores;
+    }
+  }
+  
