67b4879650749abf3e1672a0e943619d6c8c1aaf
YukkiRDZ <thiiagorodriguez.sk8@gmail.com>
2024-12-02 12:34:01 -0300

    RegistroID en la interface

--- Veterinaria/Interface.ts
+++ Veterinaria/Interface.ts
@@ -1,6 +1,6 @@
-export interface Registro<T, ID> {
+export interface Registro<T> {
     //<T> es un tipo generico, se reemplaza por el necesario en cada clase;
-    registrarse(lista: T[], ID: ID[]): void;
+    registrarse(lista: T[], ID: number[]): void;
     darBaja(lista: T[]): void;
     modRegistro(lista: T[], datosAModificar: object): void;
     generarID(): number;
