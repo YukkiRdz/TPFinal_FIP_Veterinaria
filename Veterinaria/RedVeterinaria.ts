e8ee556120e51bacc0e07d3d7858eaced78c59ab
YukkiRDZ <thiiagorodriguez.sk8@gmail.com>
2024-12-03 20:47:41 -0300

    getters + constructor

--- Veterinaria/RedVeterinaria.ts
+++ Veterinaria/RedVeterinaria.ts
@@ -2,23 +2,29 @@ import { Veterinaria } from "./Veterinaria";
 import { Proveedor } from "./Proveedor";
 
 export class RedVeterinaria {
-    private arrayVeterinarias: Veterinaria[] = [];
-    private arrayProveedores: Proveedor[] = [];
-    protected registroID: number[] = [];
+  private nombre: string;
+  private registroVeterinarias: Veterinaria[] = [];
+  private registroProveedores: Proveedor[] = [];
+  private registroID: number[] = [];
   
-    listadoVeterinarias(): Veterinaria[] {
-      return this.arrayVeterinarias;
-    }
-  
-    listadoProveedores(): Proveedor[] {
-      return this.arrayProveedores;
-    }
-  
-    agregarVeterinaria(veterinaria: Veterinaria): void {
-      this.arrayVeterinarias.push(veterinaria);
-    }
-  
-    agregarProveedor(proveedor: Proveedor): void {
-      this.arrayProveedores.push(proveedor);
-    }
+  constructor(nombre: string) {
+    this.nombre = nombre;
   }
+
+//getters
+  public getNombre(): string {
+    return this.nombre;
+  }
+
+  public listadoVeterinarias(): Veterinaria[] {
+    return this.registroVeterinarias;
+  }
+  
+  public listadoProveedores(): Proveedor[] {
+    return this.registroProveedores;
+  }
+
+  public listadoID(): number[] {
+    return this.registroID;
+  }
+}
