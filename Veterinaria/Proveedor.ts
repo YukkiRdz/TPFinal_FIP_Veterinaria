7728958dd68a6faa61dc3bdfc33ac36d31fb6b5c
YukkiRDZ <thiiagorodriguez.sk8@gmail.com>
2024-12-03 19:58:39 -0300

    Metodos de registro Proveedor

--- Veterinaria/Proveedor.ts
+++ Veterinaria/Proveedor.ts
@@ -1,4 +1,3 @@
-import { Registros } from "./Registros";
 import { Registro } from "./Interface";
 import { RedVeterinaria } from "./RedVeterinaria";
 
@@ -6,16 +5,10 @@ export class Proveedor implements Registro<Proveedor> {
   private nombre: string;
   private telefono: number;
   private ID: number | null = null;
-  private redVeterinaria: RedVeterinaria;
 
-  constructor(
-    nombre: string,
-    telefono: number,
-    redVeterinaria: RedVeterinaria
-  ) {
+  constructor(nombre: string, telefono: number) {
     this.nombre = nombre;
     this.telefono = telefono;
-    this.redVeterinaria = redVeterinaria;
   }
 
   //getters
@@ -35,7 +28,7 @@ export class Proveedor implements Registro<Proveedor> {
 
   public setNombre(nombre: string): void {
     if(!nombre){
-        throw new Error('Nombre invalido');
+      throw new Error('Nombre invalido');
     }
     this.nombre = nombre;
   }
@@ -47,36 +40,60 @@ export class Proveedor implements Registro<Proveedor> {
     this.telefono = telefono;
   }
 
-  registrarse(Proveedor: Proveedor[]): number {
-    this.ID = Math.floor(Math.random() * 100); //3
-    console.log(`Proveedor registrado con ID: ${this.ID}`);
-    this.redVeterinaria.agregarProveedor(this);
-    return this.ID;
-  }
+  //metodo de registro de proveedor
+  registrarse(registroProveedores: Proveedor[], registroID: number[]): void {
+    //verifica si el proveedor esta registrado o no;
+    const proveedorRegistrado = registroProveedores.find(proveedor => proveedor.getNombre() === this.nombre && proveedor.getTelefono() === this.telefono);
 
+    //si el proveedor ya fue registrado;
+    if(proveedorRegistrado) {
+        console.error(`El proveedor ${proveedorRegistrado.getNombre()} ya está registrado. Su ID es ${proveedorRegistrado.getID()}`);
+        return;
+        }
+
+    //genera el ID unico para cada proveedor;
+    let nuevoID: number;
+    
+    //mientras el nuevoID ya haya sido registrado;
+    while(registroID.includes(nuevoID)) {
+        //genera un ID nuevamente;
+        nuevoID = this.generarID();
+    }
+
+    this.ID = nuevoID; //ID asignado a cada proveedor;
+    registroID.push(this.ID); //almacena el ID correspondiente;
+    registroProveedores.push(this); //almacena los proveedors registrados;
+
+    console.log(`El proveedor ${this.nombre} ha sido registrado exitosamente. Su ID es ${this.ID}`);
+}
+
+//metodo para generar IDs randoms;
+generarID(): number {
+    return Math.floor(Math.random() * 100000); //Genera un ID random;
+}
   darBaja(registroProveedores: Proveedor[]): void {
-      //verifica si el cliente esta registrado o no;
+      //verifica si el proveedor esta registrado o no;
       const proveedorRegistrado = registroProveedores.findIndex(proveedor => proveedor.getID() === this.ID);
-      //si el cliente esta registrado, lo elimina del array;
+      //si el proveedor esta registrado, lo elimina del array;
       if (proveedorRegistrado >= 0) {
           registroProveedores.splice(proveedorRegistrado, 1);
           console.log(`El proveedor ${this.nombre} con ID ${this.ID} ha sido eliminado.`);
       } else {
-          console.error(`El cliente con ID ${this.ID} no ha sido encontrado. Intente nuevamente`);
+          console.error(`El proveedor con ID ${this.ID} no ha sido encontrado. Intente nuevamente`);
       }
   }
 
   modRegistro(registroProveedores: Proveedor[], datosAModificar: { nombre?: string; telefono?: number}): void {
-    //verifica si el cliente esta registrado o no;
+    //verifica si el proveedor esta registrado o no;
     const indexProveedorRegistrado = registroProveedores.findIndex(proveedor => proveedor.getID() === this.ID);
     //si el proveedor esta registrado, modifica los datos ingresados;
     if (indexProveedorRegistrado >= 0) {
         const proveedorMod = registroProveedores[indexProveedorRegistrado];
         if (datosAModificar.nombre) proveedorMod.setNombre(datosAModificar.nombre);
         if (datosAModificar.telefono) proveedorMod.setTelefono(datosAModificar.telefono);
-        console.log(`El cliente ${this.nombre} con ID ${this.ID} ha sido modificado exitosamente. Sus nuevos datos son ${this}`);
+        console.log(`El proveedor ${this.nombre} con ID ${this.ID} ha sido modificado exitosamente. Sus nuevos datos son ${this}`);
     } else {
-        console.error(`El cliente con ID ${this} no ha sido encontrado. Intente nuevamente`);
+        console.error(`El proveedor con ID ${this} no ha sido encontrado. Intente nuevamente`);
     }
   }
 }
