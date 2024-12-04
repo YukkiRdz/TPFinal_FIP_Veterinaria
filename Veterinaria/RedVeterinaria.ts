e17e893a6513d73e401f39786512fcea88e5e6b7
YukkiRDZ <thiiagorodriguez.sk8@gmail.com>
2024-12-04 10:51:52 -0300

    MostrarVeterinarias + MostrarProveedores

--- Veterinaria/RedVeterinaria.ts
+++ Veterinaria/RedVeterinaria.ts
@@ -27,4 +27,31 @@ export class RedVeterinaria {
   public listadoID(): number[] {
     return this.registroID;
   }
+
+  public mostrarProveedores(): void {
+    if (this.registroProveedores.length === 0) {
+      console.log("No hay proveedores registrados.");
+    } else {
+      console.log("Proveedores registrados:");
+      this.registroProveedores.forEach((proveedor, index) => {
+        console.log(
+          `${index + 1}. Proveedor: ${proveedor.getNombre()}, Telefono: ${proveedor.getTelefono()}, ID: ${proveedor.getID()}`
+        );
+      });
+    }
+  }
+  
+  public mostrarVeterinarias(): void {
+    if (this.registroVeterinarias.length === 0) {
+      console.log("No hay veterinarias registradas.");
+    } else {
+      console.log("Veterinarias registradas:");
+      this.registroVeterinarias.forEach((veterinaria, index) => {
+        console.log(
+          `${index + 1}. Veterinaria: ${veterinaria.getNombre()}, Dirección: ${veterinaria.getDireccion()}, Teléfono: ${veterinaria.getTelefono()}, ID: ${veterinaria.getID()}`
+        );
+      });
+    }
+  }
+  
 }
